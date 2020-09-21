f7c9233a43f85436aea40d4b5add9db6231f77b8
Kenji Fujiwara <niyaton@gmail.com>
2020-09-21 18:04:53 +0900

    add gh-pages to deploy

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
+  siteUrl: 'https://niyaton.github.io',
   siteName: 'Kenji Fujiwara',
   metadata: createMetadata(),
   plugins: []
